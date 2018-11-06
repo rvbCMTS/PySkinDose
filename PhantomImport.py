95df9cd3a377afeacd5d71e826a209970eebef83
Max Hellström <max.hellstrom@gmail.com>
2018-11-06 15:03:58 +0100

    made the cylinder elliptical

--- PhantomImport.py
+++ PhantomImport.py
@@ -5,42 +5,45 @@ import numpy as np
 from typing import List, Dict, Optional
 
 
-def create_table(table_size: dict) -> dict:
+def create_table(table_dim: dict) -> dict:
     """Creates the eight vertices of the patient support table.
 
-    :param table_size: Patient support table dimensions (width, length, thickness)
-    :type table_size: Dict[str, int]
+    :param table_dim: Patient support table dimensions (width, length, thickness)
+    :type table_dim: Dict[str, int]
     :return: (x,y,z) coordinates of the vertices of the patient support table
     :rtype: Dict[str, str]
     """
-    x = [-0.5*table_size["width"], 0.5*table_size["width"], 0.5*table_size["width"], -0.5*table_size["width"]]
-    y = [0, 0, table_size["length"], table_size["length"]]
+    x = [index * 0.5*table_dim["width"] for index in [-1, 1, 1, -1]]
+    y = [index * table_dim["length"] for index in [0, 0, 1, 1]]
     z = [0, 0, 0, 0]
     x = x + x
     y = y + y
-    z = z + [-table_size["thickness"]] * len(z)
+    z = z + [-table_dim["thickness"]] * len(z)
 
     output = {"type": "table", "x": x, "y": y, "z": z}
 
     return output
 
 
-def create_phantom(phantom_type: str, table_size: dict, cylinder_radius: int = 20) -> dict:
+def create_phantom(phantom_type: str, phantom_dim: Optional[dict] = None) -> dict:
     """creates skin dose calculation phantom.
 
     :param phantom_type: Type of phantom to create: "plane" (1D slab), "cylinder", "human" (MakeHuman binary stl)
     :type phantom_type: str
-    :param table_size: Patient support table dimensions (width, length, thickness)
-    :type table_size: Dict[str, int]
-    :param cylinder_radius: Radius of cylindrical phantom
-    :type cylinder_radius: int
+    :param phantom_dim: size of the phantom for phantom_type: "plane" (width, length) and "cylinder" (radius, length)
+    :type phantom_dim: Dict[str, int]
     :return: (x,y,z) coordinates for all point on the phantom. Also, (i,j,k) interpolation order for MakeHuman phantom
     :rtype: Dict[str, str]
     """
+
+    if phantom_type in ["plane", "cylinder"]:
+        if phantom_dim is None:
+            raise ValueError("Phantom dimensions are needed yo create a plane or cylinder phantom.")
+
     if phantom_type == "plane":
 
-        x_range = np.linspace(-0.5*table_size["width"], 0.5*table_size["width"], table_size["width"] + 1)
-        y_range = np.linspace(0, table_size["length"], table_size["length"] + 1)
+        x_range = np.linspace(-0.5*phantom_dim["width"], 0.5*phantom_dim["width"], phantom_dim["width"] + 1)
+        y_range = np.linspace(0, phantom_dim["length"], phantom_dim["length"] + 1)
         x, y = np.meshgrid(x_range, y_range)
 
         x = x.ravel().tolist()
@@ -53,12 +56,12 @@ def create_phantom(phantom_type: str, table_size: dict, cylinder_radius: int = 2
     elif phantom_type == "cylinder":
 
         t = np.arange(0, 2 * np.pi, 0.15)
-        x = (cylinder_radius * np.cos(t)).tolist()
-        z = (cylinder_radius * np.sin(t)).tolist()
+        x = (phantom_dim["a"] * np.cos(t)).tolist()
+        z = (phantom_dim["b"] * np.sin(t)).tolist()
 
         output = {"type": "cylinder", "x": [], "y": [], "z": []}
 
-        for index in range(0, table_size["length"], 10 + 1):
+        for index in range(0, phantom_dim["length"], 10 + 1):
             output["x"] = output["x"] + x
             output["z"] = output["z"] + z
             output["y"] = output["y"] + [index] * len(x)
@@ -83,7 +86,7 @@ def create_phantom(phantom_type: str, table_size: dict, cylinder_radius: int = 2
     return output
 
 
-def plot_phantom(phantom_dict: dict, include_table: bool, table_dict: Optional[dict] = None) -> List[go.Mesh3d]:
+def plot_phantom(phantom_dict: dict, include_table: bool, table_dict: Optional[dict] = None):
     """creates and plots an (offline) plotly graph of the phantom and support table (optional)
 
     :param phantom_dict: (x,y,z) coordinates for all point on the phantom.
@@ -94,39 +97,32 @@ def plot_phantom(phantom_dict: dict, include_table: bool, table_dict: Optional[d
     :param table_dict: (x,y,z) coordinates of the vertices of the patient support table
     :type table_dict: Dict[str, str]
     """
+
     if include_table:
         if table_dict is None:
             raise ValueError('Table measurements must be given when include_table is True')
 
-    if phantom_dict["type"] == "human":
+    if phantom_dict["type"] == "plane":
         phantom_mesh = [
             go.Mesh3d(
-                x=phantom_dict["x"], y=phantom_dict["y"], z=phantom_dict["z"],
-                i=phantom_dict["i"], j=phantom_dict["j"], k=phantom_dict["k"],
-                intensity=phantom_dict["dose"],
-                colorscale='Jet',
-                name='Phantom',
-                showscale=True)]
+                x=phantom_dict["x"], y=phantom_dict["y"], z=phantom_dict["z"], intensity=phantom_dict["dose"],
+                alphahull=-1, colorscale='Jet', name='plane phantom', showscale=True)]
 
     elif phantom_dict["type"] == "cylinder":
         phantom_mesh = [
             go.Mesh3d(
-                x=phantom_dict["x"], y=phantom_dict["y"], z=phantom_dict["z"],
-                alphahull=1,
-                intensity=phantom_dict["dose"],
-                colorscale='Jet',
-                name='Phantom',
-                showscale=True)]
+                x=phantom_dict["x"], y=phantom_dict["y"], z=phantom_dict["z"], intensity=phantom_dict["dose"],
+                alphahull=1, colorscale='Jet', name='Cylinder phantom', showscale=True)]
 
-    elif phantom_dict["type"] == "plane":
+    elif phantom_dict["type"] == "human":
         phantom_mesh = [
             go.Mesh3d(
-                x=phantom_dict["x"], y=phantom_dict["y"], z=phantom_dict["z"],
-                alphahull=-1,
-                intensity=phantom_dict["dose"],
-                colorscale='Jet',
-                name='Phantom',
-                showscale=True)]
+                x=phantom_dict["x"], y=phantom_dict["y"], z=phantom_dict["z"], intensity=phantom_dict["dose"],
+                i=phantom_dict["i"], j=phantom_dict["j"], k=phantom_dict["k"],
+                colorscale='Jet', name='Human phantom', showscale=True)]
+
+    else:
+        raise ValueError("Unsupported phantom type selected. Valid selections: plane, cylinder, human")
 
     layout = go.Layout(
         scene=dict(
@@ -142,16 +138,12 @@ def plot_phantom(phantom_dict: dict, include_table: bool, table_dict: Optional[d
         table_mesh = [
             go.Mesh3d(
                 x=table_dict["x"], y=table_dict["y"], z=[x + np.amin(phantom_dict["z"]) for x in table_dict["z"]],
-                alphahull=1,
-                intensity=phantom_dict["dose"],
-                color='gray',
-                opacity=0.5,
-                name='Table')]
+                intensity=phantom_dict["dose"], alphahull=1, color='gray', opacity=0.8, name='Table', showscale=False)]
 
         phantom_temp = go.Figure(data=phantom_mesh)
         table_temp = go.Figure(data=table_mesh)
 
-        fig = dict(data=[phantom_temp.data[0], table_temp.data[0]], layout=layout)
+        fig = dict(data=[table_temp.data[0], phantom_temp.data[0]], layout=layout)
         ply.plot(fig, filename='PhantomImport.html')
 
     else:
@@ -160,21 +152,17 @@ def plot_phantom(phantom_dict: dict, include_table: bool, table_dict: Optional[d
 
 
 # user commands
-table_measurements = {'width': 70, 'length': 250, 'thickness': 2}
+table_measurements = {'width': 70, 'length': 250, 'thickness': 5}
+
+phantom_measurements = {'width': 60, 'length': 180, 'radius': 20, "a": 20, "b": 10}
 
 # create phantom
 phantom = create_phantom(phantom_type='human',
-                         table_size=table_measurements,
-                         cylinder_radius=20)
+                         phantom_dim=phantom_measurements)
 
 # create table
 table = create_table(table_measurements)
 
-
-
 # plot phantom
-plot_phantom(phantom_dict=phantom,
-             include_table=True,
-             table_dict=table)
+plot_phantom(phantom_dict=phantom, include_table=True, table_dict=table)
 
-a = 1
