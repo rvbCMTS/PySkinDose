73bf5499acaf313a22d7e0e7ad18274e0bfaa7c7
Max Hellström <max.hellstrom@gmail.com>
2018-11-26 14:23:18 +0100

    Finishing commenting before merge to general.

--- PhantomImport.py
+++ PhantomImport.py
@@ -13,8 +13,11 @@ def create_table(table_dim: dict) -> dict:
     :return: (x,y,z) coordinates of the vertices of the patient support table
     :rtype: Dict[str, str]
     """
+    # Longitudinal position of the the vertices
     x = [index * 0.5*table_dim["width"] for index in [-1, 1, 1, -1, -1, 1, 1, -1]]
+    # Lateral position of the the vertices
     y = [index * table_dim["length"] for index in [0, 0, 1, 1, 0, 0, 1, 1]]
+    # Vertical position of the vertices
     z = [index * -table_dim["thickness"] for index in [0, 0, 0, 0, 1, 1, 1, 1]]
 
     output = {"type": "table", "x": x, "y": y, "z": z}
@@ -49,24 +52,24 @@ def create_phantom(phantom_type: str, human_model: Optional[str] = None, phantom
     # Creates a coordinate grid on a 2D plane for plane phantom representation
     if phantom_type == "plane":
 
-        # Linearly spaced point along the lateral direction, in steps of 1 cm
-        x_range = np.linspace(-0.5*phantom_dim["width"], 0.5*phantom_dim["width"], phantom_dim["width"] + 1)
         # Linearly spaced point along the longitudinal direction, in steps of 1 cm
+        x_range = np.linspace(-0.5*phantom_dim["width"], 0.5*phantom_dim["width"], phantom_dim["width"] + 1)
+        # Linearly spaced point along the lateral direction, in steps of 1 cm
         y_range = np.linspace(0, phantom_dim["length"], phantom_dim["length"] + 1)
-        # Create grid
+        # Create phantom in form of rectangular grid
         x, y = np.meshgrid(x_range, y_range)
 
         x = x.ravel().tolist()
         y = y.ravel().tolist()
 
-        # Preallocate memory for skin mapping
+        # Preallocate memory for skin dose mapping
         z = [0] * len(x)
         dose = [0] * len(x)
 
         # Store the  coordinates of the plane phantom, and preallocate memory for skin dose mapping at each point
         output = {"type": "plane", 'x': x, 'y': y, 'z': z, "dose": dose}
 
-    # Creates a coordinate grid along an elliptic cylinder for cylinder phantom representation
+    # Creates a coordinate grid along an elliptic cylinder for elliptic cylinder phantom representation
     elif phantom_type == "cylinder":
 
         # Creates linearly spaced points along an ellipse in the lateral direction
@@ -77,7 +80,8 @@ def create_phantom(phantom_type: str, human_model: Optional[str] = None, phantom
         # Store the  coordinates of the cylinder phantom
         output = {"type": "cylinder", "x": [], "y": [], "z": []}
 
-        # Map the ellipse
+        # Extend the ellipse to span the entire length of the phantom, in steps of 10 cm,
+        # thus creating a phantom in form of an elliptic cylinder
         for index in range(0, phantom_dim["length"], 10):
             output["x"] = output["x"] + x
             output["z"] = output["z"] + z
@@ -85,8 +89,10 @@ def create_phantom(phantom_type: str, human_model: Optional[str] = None, phantom
 
         output['dose'] = [0] * len(output['x'])
 
+    # Creates a coordinate grid on a 3D human model for human phantom representation
     elif phantom_type == "human":
 
+        # load selected phantom model from binary .stl file
         phantom_mesh = mesh.Mesh.from_file('phantom_data/'+human_model+'.stl')
         x = [el for el_list in phantom_mesh.x for el in el_list]
         y = [el for el_list in phantom_mesh.y for el in el_list]
@@ -95,9 +101,12 @@ def create_phantom(phantom_type: str, human_model: Optional[str] = None, phantom
         j = np.arange(1, len(y) - 2, 3)
         k = np.arange(2, len(z) - 1, 3)
 
+        # Store the coordinates of the human phantom
         output = {"type": "human", 'x': x, 'y': y, 'z': z, 'i': i, 'j': j, 'k': k}
+        # Preallocate memory for skin dose mapping
         output['dose'] = [0] * len(output['x'])
 
+    # Raise error if invalid phantom type selected
     else:
         raise ValueError("Unknown phantom type selected. Valid type: plane, cylinder, human")
 
@@ -115,23 +124,26 @@ def plot_phantom(phantom_dict: dict, include_table: bool, table_dict: Optional[d
     :param table_dict: (x,y,z) coordinates of the vertices of the patient support table
     :type table_dict: Dict[str, str]
     """
-
+    # Raise error if table dimensions are missing when table presentation are selected
     if include_table:
         if table_dict is None:
             raise ValueError('Table measurements must be given when include_table is True')
 
+    # Create Plotly 3D mesh object for plane phantom representation
     if phantom_dict["type"] == "plane":
         phantom_mesh = [
             go.Mesh3d(
                 x=phantom_dict["x"], y=phantom_dict["y"], z=phantom_dict["z"], intensity=phantom_dict["dose"],
                 alphahull=-1, colorscale='Jet', name='plane phantom', showscale=True)]
 
+    # Create Plotly 3D mesh object for elliptic cylinder phantom representation
     elif phantom_dict["type"] == "cylinder":
         phantom_mesh = [
             go.Mesh3d(
                 x=phantom_dict["x"], y=phantom_dict["y"], z=phantom_dict["z"], intensity=phantom_dict["dose"],
                 alphahull=1, colorscale='Jet', name='Cylinder phantom', showscale=True)]
 
+    # Create Plotly 3D mesh object for human phantom representation
     elif phantom_dict["type"] == "human":
         phantom_mesh = [
             go.Mesh3d(
@@ -139,9 +151,11 @@ def plot_phantom(phantom_dict: dict, include_table: bool, table_dict: Optional[d
                 i=phantom_dict["i"], j=phantom_dict["j"], k=phantom_dict["k"],
                 colorscale='Jet', name='Human phantom', showscale=True)]
 
+    # Raise error if invalid phantom type selected
     else:
         raise ValueError("Unsupported phantom type selected. Valid selections: plane, cylinder, human")
 
+    # Plot settings
     layout = go.Layout(
         scene=dict(
             aspectmode="data",
@@ -152,30 +166,41 @@ def plot_phantom(phantom_dict: dict, include_table: bool, table_dict: Optional[d
             zaxis=dict(
                 title='VERT [cm])')))
 
+    # If the patient support table are to be visualized in the figure
     if include_table:
+        # Create Plotly 3D mesh object for the patient support table.
         table_mesh = [
             go.Mesh3d(
                 x=table_dict["x"], y=table_dict["y"], z=[x + np.amin(phantom_dict["z"]) for x in table_dict["z"]],
                 intensity=phantom_dict["dose"], alphahull=1, color='gray', opacity=0.8, name='Table', showscale=False)]
 
+        # Fixing problem with plotting several Plotly 3D mesh object in the same figure (both table and phantom)
         phantom_temp = go.Figure(data=phantom_mesh)
         table_temp = go.Figure(data=table_mesh)
 
         fig = dict(data=[table_temp.data[0], phantom_temp.data[0]], layout=layout)
+        # Plot the result locally (No Plotly login required)
         ply.plot(fig, filename='PhantomImport.html')
 
+    # If only the patient phantom should be visualized (without patient support table
     else:
         fig = go.Figure(data=phantom_mesh, layout=layout)
+        # Plot the result locally (No Plotly login required)
         ply.plot(fig, filename='plot_phantom.html')
 
-
 # user commands
+
+
+# Define table dimensions in the longitudinal (width), lateral (length) and vertical (thickness) direction
 table_measurements = {'width': 70, 'length': 200, 'thickness': 5}
 
-phantom_measurements = {'width': 60, 'length': 180, 'radius': 20, "a": 20, "b": 10}
+
+# Define width (of the plane phantom), length (of the plane/elliptic cylinder),
+# foci a and b (elliptic cylinder) in the lateral and longitudinal direction
+phantom_measurements = {'width': 60, 'length': 180, "a": 20, "b": 10}
 
 # create phantom
-phantom = create_phantom(phantom_type='cylinder',
+phantom = create_phantom(phantom_type='human',
                          human_model='adult_male',
                          phantom_dim=phantom_measurements)
 
