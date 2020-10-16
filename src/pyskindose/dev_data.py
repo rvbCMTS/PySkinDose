31ed8e8d385e245a18b6da747492468ff1d1f905
Max Hellström <max.hellstrom@umu.se>
2020-10-17 01:58:32 +0200

    updates in constants

--- src/pyskindose/dev_data.py
+++ src/pyskindose/dev_data.py
@@ -1,17 +1,21 @@
 from . import constants as const
 
+
 DEVELOPMENT_PARAMETERS = dict(
     # modes: 'calculate_dose', 'plot_setup', 'plot_event', 'plot_procedure'
-    mode=const.MODE_PLOT_SETUP,
+    mode=const.MODE_PLOT_PROCEDURE,
     # RDSR filename
-    rdsr_filename='S1.dcm',
+    rdsr_filename='s1.dcm',
     # Irrading event index for mode='plot_event'
     plot_event_index=12,
     # Set True to estimate table correction, or False to use measured k_tab
-    # from /table_data/table_transmission.csv
     estimate_k_tab=False,
-    # Numeric value of estimated table correction
+    # Numeric value of estimated table correction    
     k_tab_val=0.8,
+    # plot settings
+    plot={
+        #dark mode for plots
+        const.MODE_DARK_MODE: True},
     # Phantom settings:
     phantom=dict(
         # Phantom model, valid selections: 'plane', 'cylinder', or 'human'
@@ -28,14 +32,16 @@ DEVELOPMENT_PARAMETERS = dict(
             const.DIMENSION_PLANE_LENGTH: 120,  # Length of plane phantom
             const.DIMENSION_PLANE_WIDTH: 40,  # Width of plane phantom
             const.DIMENSION_PLANE_RESOLUTION: const.RESOLUTION_SPARSE,  # Resolution of plane phantom
-            'cylinder_length': 150,  # Length of cylinder phantom
-            'cylinder_radii_a': 20,  # First radii of cylinder phantom
-            'cylinder_radii_b': 10,  # Second radii of cylinder phantom
-            'cylinder_resolution': 'sparse',  # Resolution of cylinder.
-            'table_thickness': 5,  # Support table thickness
-            'table_length': 210,  # Support table length
-            'table_width': 50,  # Support table width
-            'pad_thickness': 4,  # Support pad thickness
-            'pad_length': 210,  # Support pad length
-            'pad_width': 50,  # Support pad width
-            'units': 'cm'}))  # unit of dimension. Only 'cm' is supported.
+            const.DIMENSION_CYLINDER_LENGTH: 150,  # Length of cylinder phantom
+            const.DIMENSION_CYLINDER_RADII_A: 20,  # First radii of cylinder phantom
+            const.DIMENSION_CYLINDER_RADII_B: 10,  # Second radii of cylinder phantom
+            const.DIMENSION_CYLINDER_RESOLUTION: const.RESOLUTION_SPARSE,  # Resolution of cylinder.
+            const.DIMENSION_TABLE_LENGTH: 260,  # Support table length
+            const.DIMENSION_TABLE_WIDTH: 50,  # Support table width
+            const.DIMENSION_TABLE_THICKNESS: 5,  # Support table thickness
+            const.DIMENSION_PAD_LENGTH: 260,  # Support pad length
+            const.DIMENSION_PAD_WIDTH: 50,  # Support pad width
+            const.DIMENSION_PAD_THICKNESS: 4,  # Support pad thickness
+            const.DIMENSION_UNIT: const.DIMENSION_UNIT_CM})  # unit of dimension. Only 'cm' is supported.
+    ,
+)
