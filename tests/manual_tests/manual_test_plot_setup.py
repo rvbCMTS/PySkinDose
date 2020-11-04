197d17b4048dc28995ea7c4facdeca1720d24182
Max Hellström <max.hellstrom@gmail.com>
2020-11-04 10:16:48 +0100

    _//_

--- tests/manual_tests/manual_test_plot_setup.py
+++ tests/manual_tests/manual_test_plot_setup.py
@@ -1,7 +1,9 @@
 from base_dev_settings import DEVELOPMENT_PARAMETERS
 from pyskindose import constants
 from pyskindose.main import main
+from pyskindose.settings_pyskindose import PyskindoseSettings
 
-DEVELOPMENT_PARAMETERS['mode'] = constants.MODE_PLOT_SETUP
+settings = PyskindoseSettings(settings=DEVELOPMENT_PARAMETERS)
+settings.mode=constants.MODE_PLOT_SETUP
 
-main(settings=DEVELOPMENT_PARAMETERS)
+main(settings=settings)
