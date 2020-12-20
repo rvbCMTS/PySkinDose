e1c40474d92198c644aaa40faba6ee0af6b889f0
Max Hellström <max.hellstrom@gmail.com>
2020-12-21 00:35:48 +0100

    typos

--- tests/manual_tests/manual_test_plot_procedure_table_translations.py
+++ tests/manual_tests/manual_test_plot_procedure_table_translations.py
@@ -6,6 +6,6 @@ from pyskindose.settings_pyskindose import PyskindoseSettings
 settings = PyskindoseSettings(settings=DEVELOPMENT_PARAMETERS)
 settings.mode = constants.MODE_PLOT_PROCEDURE
 
-settings.rdsr_filename = 'table_translation.json'
+settings.rdsr_filename = 'table_translations.json'
 
 main(settings=settings)
