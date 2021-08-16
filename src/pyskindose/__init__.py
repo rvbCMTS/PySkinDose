7eaa011bc0a6e1b44e4ba484b33ec42ca22c2e69
Max Hellström <56262357+MaxHellstrom@users.noreply.github.com>
2021-08-16 12:55:02 +0200

    Reformat coordinate transforms (#17)
    * ignore png images in repo
    
    * Made the transforms easier to understand and fixed incorrect ap3
    
    * changed name on test (since ap3 changed)
    
    * rename function to better correspond to the new coordinate system
    
    * _//_
    
    * new phantoms (changed orientation and direction)
    
    * reformat positioning function
    
    * added unit test for patient positioning
    
    * removed comment
    
    * updated docstring in geom_calc
    
    * removed commented out code
    
    * updated plotly to solve beam opacity issue on safari browser
    
    * updated pillow to solve security issue
    
    * updates (more readable) code for create_wireframe
    
    * moved plotly ijk indexing to separate function
    
    * improved patient table and pad indexing
    
    * fixed check_table_hit triangles with new coordinate standard
    
    * fixed incorrect reference to documentation
    
    * fixed typo

--- src/pyskindose/__init__.py
+++ src/pyskindose/__init__.py
@@ -1,5 +1,5 @@
 from .beam_class import Beam
-from .geom_calc import position_geometry, scale_field_area, fetch_and_append_hvl, check_new_geometry
+from .geom_calc import position_patient_phantom_on_table, scale_field_area, fetch_and_append_hvl, check_new_geometry
 from .rdsr_parser import rdsr_parser
 from .rdsr_normalizer import rdsr_normalizer
 from .phantom_class import Phantom
---
+++ src/pyskindose/plotting/create_ploty_ijk_indices.py
@@ -0,0 +1,66 @@
+
+def _create_plotly_ijk_indices_for_cuboid_objects():
+
+    # create list for storing indices order
+    i_tab = []
+    j_tab = []
+    k_tab = []
+
+    # append triangles to fill cuboid top side
+    i_tab.append(0)
+    j_tab.append(1)
+    k_tab.append(2)
+
+    i_tab.append(2)
+    j_tab.append(3)
+    k_tab.append(0)
+
+    # append triangles to fill cuboid bottom side
+    i_tab.append(0 + 4)
+    j_tab.append(1 + 4)
+    k_tab.append(2 + 4)
+
+    i_tab.append(2 + 4)
+    j_tab.append(3 + 4)
+    k_tab.append(0 + 4)
+
+
+
+    # append triangles to fill cuboid right side
+    i_tab.append(0)
+    j_tab.append(1)
+    k_tab.append(4)
+
+    i_tab.append(1)
+    j_tab.append(5)
+    k_tab.append(4)
+
+    # append triangles to fill cuboid left side
+    i_tab.append(2)
+    j_tab.append(3)
+    k_tab.append(6)
+
+    i_tab.append(3)
+    j_tab.append(7)
+    k_tab.append(6)
+
+    # append triangles to fill cuboid head side
+    i_tab.append(0)
+    j_tab.append(3)
+    k_tab.append(4)
+
+    i_tab.append(3)
+    j_tab.append(7)
+    k_tab.append(4)
+
+
+    # append triangles to fill cuboid feet side
+    i_tab.append(1)
+    j_tab.append(2)
+    k_tab.append(5)
+
+    i_tab.append(2)
+    j_tab.append(6)
+    k_tab.append(5)
+
+    return i_tab, j_tab, k_tab
