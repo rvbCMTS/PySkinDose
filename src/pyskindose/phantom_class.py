73bcd8a0b5e5a6a283aacfee319631fbbab752c0
Max Hellström <max.hellstrom@gmail.com>
2022-03-08 09:19:53 +0100

    removed unused import

--- src/pyskindose/phantom_class.py
+++ src/pyskindose/phantom_class.py
@@ -4,7 +4,6 @@ from typing import Dict, List, Optional
 
 import numpy as np
 import pandas as pd
-import plotly.graph_objects as go
 from stl import mesh
 
 from pyskindose.plotting.create_ploty_ijk_indices import (
