2cbcfb4a91922048e06d91200ab0d80383495db7
MaxHellstrom <max.hellstrom@gmail.com>
2023-06-03 13:21:54 +0200

    added missing settings to test

--- tests/integrationtests/test_corrections.py
+++ tests/integrationtests/test_corrections.py
@@ -4,10 +4,10 @@ from pyskindose.geom_calc import fetch_and_append_hvl
 from pyskindose.rdsr_normalizer import rdsr_normalizer
 
 def test_that_hvl_can_be_fetched_from_correction_database(
-    allura_parsed, axiom_artis_parsed):
+    allura_parsed, axiom_artis_parsed, example_settings):
 
-    data_norm_philips: pd.DataFrame = rdsr_normalizer(data_parsed=allura_parsed)
-    data_norm_siemens: pd.DataFrame = rdsr_normalizer(data_parsed=axiom_artis_parsed)
+    data_norm_philips: pd.DataFrame = rdsr_normalizer(data_parsed=allura_parsed, settings=example_settings)
+    data_norm_siemens: pd.DataFrame = rdsr_normalizer(data_parsed=axiom_artis_parsed, settings=example_settings)
 
     datas = [data_norm_philips, data_norm_siemens]
     actual = []
