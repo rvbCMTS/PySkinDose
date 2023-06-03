375dd9c2de291c0790b53c0c9a7f66f110e02009
Josef Lundman <josef.lundman@gmail.com>
2023-06-03 14:32:15 +0200

    Updates programming language support in setup.py and README

--- setup.py
+++ setup.py
@@ -18,7 +18,10 @@ setup(
     license="MIT",
     classifiers=[
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     packages=find_packages(where="src"),
     package_dir={"": "src"},
