Fix bug introduced in optmisation of calculation of rotation matrices

src/pyskindose/helpers/calculate_rotation_matrices.py
@@ -3,10 +3,10 @@ import pandas as pd
 
 
 def calculate_rotation_matrices(normalized_data: pd.DataFrame) -> pd.DataFrame:
-
-    at1_ind = normalized_data.columns.get_loc("At1")  # Table Horizontal Rotation Angle, rotation axis: center of table
-    at2_ind = normalized_data.columns.get_loc("At2")  # Table Head Tilt Angle, rotation axis: center of table
-    at3_ind = normalized_data.columns.get_loc("At3")  # Table Cradle Tilt Angle
+    at = normalized_data.loc[:, ["At1", "At2", "At3"]]
+    at.At1 = [np.deg2rad(val) for val in at["At1"]]  # Table Horizontal Rotation Angle, rotation axis: center of table
+    at.At2 = [np.deg2rad(val) for val in at["At2"]]  # Table Head Tilt Angle, rotation axis: center of table
+    at.At3 = [np.deg2rad(val) for val in at["At3"]]  # Table Cradle Tilt Angle
 
     return normalized_data.join(
         pd.DataFrame(
@@ -14,21 +14,21 @@ def calculate_rotation_matrices(normalized_data: pd.DataFrame) -> pd.DataFrame:
                 (
                     [
                         [+1, +0, +0],
-                        [+0, +float(np.cos(np.deg2rad(row[at2_ind]))), -float(np.sin(np.deg2rad(row[at2_ind])))],
-                        [+0, +float(np.sin(np.deg2rad(row[at2_ind]))), +float(np.cos(np.deg2rad(row[at2_ind])))],
+                        [+0, +float(np.cos(row.At2)), -float(np.sin(row.At2))],
+                        [+0, +float(np.sin(row.At2)), +float(np.cos(row.At2))],
                     ],
                     [
-                        [+float(np.cos(np.deg2rad(row[at1_ind]))), +0, +float(np.sin(np.deg2rad(row[at1_ind])))],
+                        [+float(np.cos(row.At1)), +0, +float(np.sin(row.At1))],
                         [+0, +1, +0],
-                        [-float(np.sin(np.deg2rad(row[at1_ind]))), +0, +float(np.cos(np.deg2rad(row[at1_ind])))],
+                        [-float(np.sin(row.At1)), +0, +float(np.cos(row.At1))],
                     ],
                     [
-                        [+float(np.cos(np.deg2rad(row[at3_ind]))), -float(np.sin(np.deg2rad(row[at3_ind]))), +0],
-                        [+float(np.sin(np.deg2rad(row[at3_ind]))), +float(np.cos(np.deg2rad(row[at3_ind]))), +0],
+                        [+float(np.cos(row.At3)), -float(np.sin(row.At3)), +0],
+                        [+float(np.sin(row.At3)), +float(np.cos(row.At3)), +0],
                         [+0, +0, +1],
                     ],
                 )
-                for row in normalized_data.itertuples()
+                for row in at.itertuples()
             ],
             columns=["Rx", "Ry", "Rz"],
         )